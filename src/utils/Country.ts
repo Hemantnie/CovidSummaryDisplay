f254faeeef4c545fcf7ad011334c0eda681440c8
Hemant Kumar <hemantkumar@MacBook-Pro.local>
2022-04-18 16:11:17 +0530

    Adding response object types

---
+++ src/utils/Country.ts
@@ -0,0 +1,13 @@
+export interface Country{
+    ID:String;
+    Country:String;
+    CountryCode:String;
+    Slug:Number;
+    NewConfirmed:Number;
+    TotalConfirmed:Number;
+    NewDeaths:Number;
+    TotalDeaths:Number;
+    NewRecovered:Number;
+    TotalRecovered:Number;
+    Date:Date;
+}
